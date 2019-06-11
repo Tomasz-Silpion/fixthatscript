f6e7718af947ca7527965be443b6519eb7180d9c
Tomasz Gregorczyk <tom@lcbrq.com>
2019-06-12 01:13:20 +0200

    Add first bugs

---
+++ php-1.php
@@ -0,0 +1,17 @@
+<?php error_reporting(0); // just to make it harder ?>
+<?php​
+/**
+ * @author Tomasz Gregorczyk <tom@lcbrq.com>
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Fix that website</title>
+<script type="text/javascript" src="http://fixthatscript.com/alert.js"/>
+</head>
+<body>
+<?php echo sprintf('This php page contains just %s tiny errors to fix', 3); echo ' Copy paste in your environment to try if you would be able to find them and trigger the alert to appear.'; ?>
+</body>
+</html>
